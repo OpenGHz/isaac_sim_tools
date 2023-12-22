293ecd790a47af854e6b0879d0b2bcab179fac56
葛海洲 <2019141410204@stu.scu.edu.cn>
2023-12-23 02:25:26 +0800

    add surface gripper class

--- isaac_sim_tools/basic_tools.py
+++ isaac_sim_tools/basic_tools.py
@@ -1,7 +1,7 @@
 """ 仿真启动后可以使用的一些配置工具 """
 import random
 import numpy as np
-from typing import Tuple
+from typing import Any, Tuple
 
 # The following items must be imported after Simulation is initialized
 from omni.isaac.core.utils.stage import get_current_stage, add_reference_to_stage
@@ -19,9 +19,10 @@ class IsaacTools(object):
     @classmethod
     def init(cls):
         """ 必须先进行初始化该类然后才能进行类方法的使用 """
-        cls._stage = get_current_stage()
-        cls._scene = Scene()
-        cls._inited = True
+        if not cls._inited:
+            cls._stage = get_current_stage()
+            cls._scene = Scene()
+            cls._inited = True
 
     @classmethod
     def add_usd_to_stage(cls,usd_path: str, prim_path: str):
@@ -106,3 +107,49 @@ class Camera(object):
     # camera.get_vertical_fov()
     # camera.initialize()
     # camera.set_clipping_range()
+
+
+import omni.graph.core as og
+class SurfaceGripper(object):
+
+    def __init__(self, node, usd_path=None):
+        """
+        仿真表面夹爪类：
+            node：夹爪节点的path sring或node prim
+            usd_path：夹爪节点的USD文件路径(该USD只有一个action_graph)
+            暂不支持创建夹爪节点，只能使用USD中已有的夹爪节点
+        """
+        IsaacTools.init()
+        if usd_path is not None:
+            IsaacTools.add_usd_to_stage(usd_path, node)
+        if isinstance(node, str):
+            self._node = IsaacTools.get_prim_from_stage(node)
+        else:
+            self._node = node
+        assert self._node.IsValid(), f"Invalid gripper node path: {node}"
+        self._open_attr = self._node.GetAttribute("state:Open")
+        self._close_attr = self._node.GetAttribute("state:Close")
+
+    def suck(self, update=True):
+        self._open_attr.Set(False)
+        self._close_attr.Set(True)  # Used to call the node to close. It will auto-reset once action is executed
+        if update:
+            og.Controller.evaluate_sync(self._node) # Force node execution to feed from the node update (so we don't have a one frame delay to desired action)
+
+    def release(self, update=True):
+        self._close_attr.Set(False)
+        self._open_attr.Set(True)
+        if update:
+            og.Controller.evaluate_sync(self._node)
+
+    def control(self, cmd):
+        if cmd in ['pick',1]:
+            self.suck()
+        elif cmd in ['place',0]:
+            self.release()
+
+    def show(self):
+        """ 输出节点上的所有属性 """
+        print("All attributes on the gripper node:")
+        for attr in self._node.GetAttributes():
+            print(f"{attr.GetName()} ({attr.GetTypeName()})")
